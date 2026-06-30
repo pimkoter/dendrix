01478d9cf8cb580225aa15bc7f60180bedf78787
pim <pim@NixBTW>
2026-06-30 20:23:00 +0200

    Checkpoint

--- modules/ux/user/pim/userConfig.nix
+++ modules/ux/user/pim/userConfig.nix
@@ -26,12 +26,69 @@
       stateVersion = "25.05";
     };
   };
+
+  # Share stylix config between NixOS and standalone HM
+  stylixConfig = {
+    options,
+    pkgs,
+    lib,
+    ...
+  }: let
+    catppuccin-wall = pkgs.fetchurl {
+      url = "https://files.orangc.net/media/walls-catppuccin-mocha/abstract-swirls.jpg";
+      sha256 = "sha256-QyvJgQ7FHLoFmeVc9HPQSnOEmT0aAEpWFblh6PDyluw=";
+    };
+  in {
+    stylix = {
+      enable = true;
+      image = catppuccin-wall;
+      polarity = "dark";
+      opacity.terminal = 0.7;
+      cursor = {
+        package = pkgs.bibata-cursors;
+        name = "Bibata-Modern-Ice";
+        size = 24;
+      };
+      fonts = {
+        monospace = {
+          package = pkgs.nerd-fonts.jetbrains-mono;
+          name = "JetBrains Mono";
+        };
+        sansSerif = {
+          package = pkgs.nerd-fonts.jetbrains-mono;
+          name = "JetBrains Mono";
+        };
+        serif = {
+          package = pkgs.nerd-fonts.jetbrains-mono;
+          name = "JetBrains Mono";
+        };
+        sizes = {
+          applications = 12;
+          terminal = 15;
+          desktop = 11;
+          popups = 12;
+        };
+      };
+      targets = {
+        nvf.enable = false;
+        # Only set NixOS targets if the option exists (i.e., we are in a NixOS module)
+      } // lib.optionalAttrs (options.stylix.targets ? plymouth) {
+        plymouth.enable = false;
+      } // lib.optionalAttrs (options.stylix.targets ? kmscon) {
+        kmscon.enable = false;
+      };
+    };
+  };
 in {
   flake.homeModules.pim = pimHomeModule;
 
   flake.homeConfigurations.pim = inputs.home-manager.lib.homeManagerConfiguration {
     pkgs = import inputs.nixpkgs {system = "x86_64-linux";};
-    modules = [pimHomeModule];
+    modules = [
+      inputs.stylix.homeManagerModules.stylix
+      stylixConfig
+      pimHomeModule
+    ];
     extraSpecialArgs = {
       inherit self inputs;
       hostName = "NixBTW"; # Default for standalone
@@ -54,7 +111,9 @@ in {
         inherit self inputs;
         hostName = config.networking.hostName;
       };
-      users.pim = pimHomeModule;
+      users.pim = {
+        imports = [pimHomeModule];
+      };
     };
 
     users.users.pim = {
