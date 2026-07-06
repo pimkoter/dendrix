a56ff4d6b124142c58be37e1397decda349e51f2
pim <pim@koter>
2026-07-06 16:32:03 +0200

    improved script

--- modules/custom/addip.sh
+++ modules/custom/addip.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-read -p "What adress would you like to add?\n" adress
+printf "What ip address would you like to add?\n"
+read -p "" adress
 sudo ip addr add dev enp6s0 ${adress}
 echo "Added ip $adress"
