9c44750359179d3f82274d31d5c9ec4072d65d1e
pim <pim@koter>
2026-07-08 14:36:23 +0200

    added ollama

---
+++ modules/system/ollama.nix
@@ -0,0 +1,17 @@
+{
+  self,
+  inputs,
+  ...
+}: {
+  flake.nixosModules.ollama = {pkgs, ...}: {
+    services = {
+      ollama = {
+        enable = true;
+        package = pkgs.ollama-cuda;
+      };
+      open-webui = {
+        enable = true;
+      };
+    };
+  };
+}
