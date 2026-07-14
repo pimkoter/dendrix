8fe6cac361cde31dacc86e400e19e47ff7aaa4db
pimkoter <pim@koter>
2026-07-14 18:35:32 +0200

    gave up on sops :(

--- flake.nix
+++ flake.nix
@@ -6,10 +6,6 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
     zennotes.url = "github:ZenNotes/zennotes";
-    sops-nix = {
-      url = "github:Mic92/sops-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
