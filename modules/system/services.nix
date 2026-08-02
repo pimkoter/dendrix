bde85e14691f79f93ebe0c4214fe2e81565168f1
pimkoter <pim@koter>
2026-08-02 22:44:29 +0200

    feature(tailscale) auto enable on boot

--- modules/system/services.nix
+++ modules/system/services.nix
@@ -1,13 +1,9 @@
 {
-  self,
-  inputs,
-  ...
-}: {
-  flake.nixosModules.services = {
+  flake.nixosModules.services = { pkgs, ... }: {
     services = {
       avahi = {
         enable = true;
-        openFirewall = true; # <-- This opens the UDP port 5353 for mDNS discovery
+        openFirewall = true;
       };
       printing = {
         enable = true;
@@ -34,6 +30,35 @@
     hardware.bluetooth = {
       enable = true;
       powerOnBoot = true;
+    };
+
+    # Auto enable tailscale on boot
+    systemd.services.tailscale-autoconnect = {
+      description = "Automatic connection to Tailscale";
+
+      # make sure tailscale is running before trying to connect to tailscale
+      after = [
+        "network-pre.target"
+        "tailscale.service"
+      ];
+      wants = [
+        "network-pre.target"
+        "tailscale.service"
+      ];
+      wantedBy = [ "multi-user.target" ];
+
+      # set this service as a oneshot job
+      serviceConfig.Type = "oneshot";
+
+      # have the job run this shell script
+      script = with pkgs; ''
+        sleep 2
+        status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
+        if [ $status = "Running" ]; then # if so, then do nothing
+          exit 0
+        fi
+        ${tailscale}/bin/tailscale up
+      '';
     };
   };
 }
