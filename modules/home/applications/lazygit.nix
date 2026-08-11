f79bfa1f1201a81b06d3f0466f69d69e6bc42cf4
pimkoter <pim@koter>
2026-08-11 14:59:03 +0200

    fix(lazygit) added NVF as default editor

--- modules/home/applications/lazygit.nix
+++ modules/home/applications/lazygit.nix
@@ -1,13 +1,10 @@
 {
-  self,
-  inputs,
-  ...
-}: {
   flake.homeModules.lazygit = {
     programs.lazygit = {
       enable = true;
       enableZshIntegration = true;
       settings = {
+        os.editPreset = "nvim";
       };
     };
   };
