6822c78af82a581173931fbae607850ea976392a
pimkoter <pim@koter>
2026-08-11 11:27:22 +0200

    feat(xonsh) added new xonsh shell config

--- modules/home/shell/tmux.nix
+++ modules/home/shell/tmux.nix
@@ -1,131 +1,134 @@
 {
-  flake.homeModules.tmux = {
-    config,
-    lib,
-    pkgs,
-    ...
-  }: let
-    c = config.lib.stylix.colors.withHashtag;
-  in {
-    programs.tmux = {
-      enable = true;
-      clock24 = true;
-      keyMode = "vi";
-      terminal = "tmux-256color";
-      shortcut = "a";
-      shell = "${pkgs.zsh}/bin/zsh";
-      baseIndex = 1;
-      newSession = true;
-      mouse = false;
-      extraConfig = ''
-        set -ga terminal-overrides ",*:RGB"
-        set -g set-clipboard on
-        set -g focus-events on
+  flake.homeModules.tmux =
+    {
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
+    let
+      c = config.lib.stylix.colors.withHashtag;
+    in
+    {
+      programs.tmux = {
+        enable = true;
+        clock24 = true;
+        keyMode = "vi";
+        terminal = "tmux-256color";
+        shortcut = "a";
+        shell = "${pkgs.xonsh}/bin/xonsh";
+        baseIndex = 1;
+        newSession = true;
+        mouse = false;
+        extraConfig = ''
+          set -ga terminal-overrides ",*:RGB"
+          set -g set-clipboard on
+          set -g focus-events on
 
-        unbind C-b
-        set -g prefix C-a
-        bind-key C-a send-prefix
+          unbind C-b
+          set -g prefix C-a
+          bind-key C-a send-prefix
 
-        ##### Splits #####
+          ##### Splits #####
 
-        unbind %
-        unbind '"'
+          unbind %
+          unbind '"'
 
-        bind | split-window -h -c "#{pane_current_path}"
-        bind - split-window -v -c "#{pane_current_path}"
+          bind | split-window -h -c "#{pane_current_path}"
+          bind - split-window -v -c "#{pane_current_path}"
 
-        bind v split-window -h -c "#{pane_current_path}"
-        bind s split-window -v -c "#{pane_current_path}"
+          bind v split-window -h -c "#{pane_current_path}"
+          bind s split-window -v -c "#{pane_current_path}"
 
-        ##### Pane Navigation #####
+          ##### Pane Navigation #####
 
-        bind h select-pane -L
-        bind j select-pane -D
-        bind k select-pane -U
-        bind l select-pane -R
+          bind h select-pane -L
+          bind j select-pane -D
+          bind k select-pane -U
+          bind l select-pane -R
 
-        # alt+hjkl to switch panes without prefix
-        bind -n M-h select-pane -L
-        bind -n M-j select-pane -D
-        bind -n M-k select-pane -U
-        bind -n M-l select-pane -R
+          # alt+hjkl to switch panes without prefix
+          bind -n M-h select-pane -L
+          bind -n M-j select-pane -D
+          bind -n M-k select-pane -U
+          bind -n M-l select-pane -R
 
-        ##### QOL configs #####
+          ##### QOL configs #####
 
-        set -g pane-base-index 1
-        set-window-option -g pane-base-index 1
-        set -g renumber-windows on
+          set -g pane-base-index 1
+          set-window-option -g pane-base-index 1
+          set -g renumber-windows on
 
-        # Shift + arrows to switch windows
-        bind -n S-Left previous-window
-        bind -n S-Right next-window
+          # Shift + arrows to switch windows
+          bind -n S-Left previous-window
+          bind -n S-Right next-window
 
-        # Alt + number to switch windows
-        bind -n M-1 select-window -t 1
-        bind -n M-2 select-window -t 2
-        bind -n M-3 select-window -t 3
-        bind -n M-4 select-window -t 4
-        bind -n M-5 select-window -t 5
-        bind -n M-6 select-window -t 6
-        bind -n M-7 select-window -t 7
-        bind -n M-8 select-window -t 8
-        bind -n M-9 select-window -t 9
-        bind -n M-0 select-window -t 10
+          # Alt + number to switch windows
+          bind -n M-1 select-window -t 1
+          bind -n M-2 select-window -t 2
+          bind -n M-3 select-window -t 3
+          bind -n M-4 select-window -t 4
+          bind -n M-5 select-window -t 5
+          bind -n M-6 select-window -t 6
+          bind -n M-7 select-window -t 7
+          bind -n M-8 select-window -t 8
+          bind -n M-9 select-window -t 9
+          bind -n M-0 select-window -t 10
 
-        ##### Copy Mode #####
+          ##### Copy Mode #####
 
-        bind-key -T copy-mode-vi v send-keys -X begin-selection
-        bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
-        bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+          bind-key -T copy-mode-vi v send-keys -X begin-selection
+          bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
+          bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
 
-        unbind -T copy-mode-vi MouseDragEnd1Pane
+          unbind -T copy-mode-vi MouseDragEnd1Pane
 
-        ##### Status bar settings #####
+          ##### Status bar settings #####
 
-        set -g status "on"
-        set -g status-bg "${c.base00}"
-        set -g status-justify "centre"
+          set -g status "on"
+          set -g status-bg "${c.base00}"
+          set -g status-justify "centre"
 
-        set -g status-left-length "1000"
-        set -g status-right-length "1000"
+          set -g status-left-length "1000"
+          set -g status-right-length "1000"
 
-        ##### Panes #####
+          ##### Panes #####
 
-        set -g pane-border-style "fg=${c.base02}"
-        set -g pane-active-border-style "fg=${c.base0D}"
+          set -g pane-border-style "fg=${c.base02}"
+          set -g pane-active-border-style "fg=${c.base0D}"
 
-        ##### Windows #####
+          ##### Windows #####
 
-        set -g window-status-activity-style "fg=${c.base09},bg=${c.base00},none"
-        set -g window-status-separator ""
-        # Set the default window style background to base02 for the tab shape
-        set -g window-status-style "fg=${c.base03},bg=${c.base02},none"
+          set -g window-status-activity-style "fg=${c.base09},bg=${c.base00},none"
+          set -g window-status-separator ""
+          # Set the default window style background to base02 for the tab shape
+          set -g window-status-style "fg=${c.base03},bg=${c.base02},none"
 
-        ##### Messages #####
+          ##### Messages #####
 
-        set -g message-style "fg=${c.base05},bg=${c.base01},align=centre"
-        set -g message-command-style "fg=${c.base05},bg=${c.base01},align=centre"
+          set -g message-style "fg=${c.base05},bg=${c.base01},align=centre"
+          set -g message-command-style "fg=${c.base05},bg=${c.base01},align=centre"
 
-        ##### Status line current window #####
+          ##### Status line current window #####
 
-        set -g window-status-current-format "#[fg=${c.base00},bg=${c.base0D}] #I: #[fg=${c.base00},bg=${c.base0D}](✓) #[fg=${c.base00},bg=${c.base0D}]#(echo '#{pane_current_path}' | rev | cut -d'/' -f-2 | rev) #[fg=${c.base00},bg=${c.base0D}]"
+          set -g window-status-current-format "#[fg=${c.base00},bg=${c.base0D}] #I: #[fg=${c.base00},bg=${c.base0D}](✓) #[fg=${c.base00},bg=${c.base0D}]#(echo '#{pane_current_path}' | rev | cut -d'/' -f-2 | rev) #[fg=${c.base00},bg=${c.base0D}]"
 
-        ##### Statusline - other windows #####
+          ##### Statusline - other windows #####
 
-        set -g window-status-format "#[fg=${c.base03},bg=${c.base02}] #I: #[fg=${c.base03},bg=${c.base02}]#W #[fg=${c.base03},bg=${c.base02}]"
+          set -g window-status-format "#[fg=${c.base03},bg=${c.base02}] #I: #[fg=${c.base03},bg=${c.base02}]#W #[fg=${c.base03},bg=${c.base02}]"
 
-        ##### Statusline - Left & Right Side overrides #####
-        # Show your session name (#S) in base05/base00 colors, followed by the vim-tpipeline left status
-        set -g status-left "#[fg=${c.base05},bg=${c.base00}] #S #[fg=default,bg=default] #(cat #{socket_path}-\#{session_id}-vimbridge)"
+          ##### Statusline - Left & Right Side overrides #####
+          # Show your session name (#S) in base05/base00 colors, followed by the vim-tpipeline left status
+          set -g status-left "#[fg=${c.base05},bg=${c.base00}] #S #[fg=default,bg=default] #(cat #{socket_path}-\#{session_id}-vimbridge)"
 
-        # Show the vim-tpipeline right status on the right side
-        set -g status-right "#(cat #{socket_path}-\#{session_id}-vimbridge-R)"
+          # Show the vim-tpipeline right status on the right side
+          set -g status-right "#(cat #{socket_path}-\#{session_id}-vimbridge-R)"
 
-        ##### Modes #####
+          ##### Modes #####
 
-        set -g clock-mode-colour "${c.base0D}"
-        set -g mode-style "fg=${c.base00} bg=${c.base0D} bold"
-      '';
+          set -g clock-mode-colour "${c.base0D}"
+          set -g mode-style "fg=${c.base00} bg=${c.base0D} bold"
+        '';
+      };
     };
-  };
 }
---
+++ modules/home/shell/xonsh.nix
@@ -0,0 +1,65 @@
+{
+  flake.homeModules.xonsh =
+    { pkgs, lib, ... }:
+
+    let
+      aliases = {
+        cd = "z";
+        cdi = "zi";
+
+        v = "nvim";
+        V = "sudo nvim";
+
+        cat = "bat";
+        find = "fd";
+        lg = "lazygit";
+
+        c = "clear";
+        e = "exit";
+
+        ssn = "sudo systemctl poweroff";
+        srn = "sudo systemctl reboot";
+
+        ff = "fastfetch";
+
+        shell = "nix-shell -p";
+        nd = "nix develop --impure -c xonsh";
+        gens = "sudo nixos-rebuild list-generations";
+
+        ls = "eza";
+        ll = "eza -lh --no-user --long";
+        la = "eza -lah --no-git";
+        tree = "eza --no-git --tree";
+      };
+
+      aliasLines = lib.concatStringsSep "\n" (
+        lib.mapAttrsToList (
+          name: command: "aliases[${builtins.toJSON name}] = ${builtins.toJSON command}"
+        ) aliases
+      );
+    in
+    {
+      home.packages = [
+        pkgs.xonsh
+      ];
+
+      home.file.".xonshrc".text = ''
+        # Init
+        execx($(starship init xonsh))
+
+        # Interactive startup
+        if $XONSH_INTERACTIVE:
+            if not __xonsh__.env.get("TMUX"):
+                import os
+                os.execvp("tmux", ["tmux"])
+
+            fastfetch
+
+        # Aliases
+        ${aliasLines}
+
+        # Zoxide
+        execx($(zoxide init xonsh), 'exec', __xonsh__.ctx, filename='zoxide')
+      '';
+    };
+}
