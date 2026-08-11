da329de05029444b1a758eb1a145ab9846ea7983
pimkoter <pim@koter>
2026-08-11 15:13:52 +0200

    fix(ly) fixed log display

--- modules/nixos/greeters/ly.nix
+++ modules/nixos/greeters/ly.nix
@@ -1,28 +1,31 @@
 {
-  flake.nixosModules.ly = {
-    config,
-    pkgs,
-    ...
-  }: {
-    # Enable the Ly Display Manager
-    services.displayManager.ly = {
-      enable = true;
+  flake.nixosModules.ly =
+    {
+      config,
+      pkgs,
+      ...
+    }:
+    {
+      # Enable the Ly Display Manager
+      services.displayManager.ly = {
+        enable = true;
 
-      # Custom configuration settings mapping to Ly's config.ini
-      settings = {
-        # Visual & Animation Customization
-        animation = 1;
-        bigclock = true;
-        blank_password = false;
-        clear_password = true;
+        # Custom configuration settings mapping to Ly's config.ini
+        settings = {
+          # Visual & Animation Customization
+          animation = 1;
+          bigclock = true;
+          blank_password = false;
+          clear_password = true;
 
-        # Environment Defaults
-        default_rm = true;
-        hide_borders = false;
+          # Environment Defaults
+          default_rm = true;
+          hide_borders = false;
 
-        # Language and Console settings
-        lang = "en";
+          # Language and Console settings
+          lang = "en";
+          log = "/var/log/ly-session.log";
+        };
       };
     };
-  };
 }
