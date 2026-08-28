190044a8b1138ca0a1c62d1cc8a8fce8483cef2a
pimkoter <pim@koter>
2026-08-29 01:23:49 +0200

    feat(flake) added Sops-nix input

--- flake.nix
+++ flake.nix
@@ -36,6 +36,10 @@
       url = "github:pimkoter/vm-curator";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    sops-nix = {
+      url = "github:mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     zen-browser = {
       url = "github:youwen5/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -47,6 +51,7 @@
       imports = [
         (inputs.import-tree ./modules)
         inputs.home-manager.flakeModules.home-manager
+        inputs.sops-nix.nixosModules.sops
       ];
       systems = [
         "x86_64-linux"
