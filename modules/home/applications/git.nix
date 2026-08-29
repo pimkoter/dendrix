86f0b2f403a95dcd8a6dcc71e9a0a12628cc3492
pimkoter <pim@koter>
2026-08-29 13:43:11 +0200

    feat(git) setup SOPS encryption for personal data

--- modules/home/applications/git.nix
+++ modules/home/applications/git.nix
@@ -1,14 +1,28 @@
 {
-  flake.homeModules.git = {
-    programs.git = {
-      enable = true;
-      settings = {
-        user.name = "pimkoter";
-        user.email = "pim@koter";
-        init.defaultBranch = "main";
-        pull.rebase = true;
-        color.ui = "auto";
+  flake.homeModules.git =
+    { config, ... }:
+    {
+      sops = {
+        secrets = {
+          "personal/git/user" = { };
+          "personal/git/email" = { };
+        };
+
+        templates."git-config".content = ''
+          [user]
+            name = "${config.sops.placeholder."personal/git/user"}"
+            email = "${config.sops.placeholder."personal/git/email"}"
+        '';
+      };
+
+      programs.git = {
+        enable = true;
+        extraConfig = {
+          include.path = config.sops.templates."git-config".path;
+          init.defaultBranch = "main";
+          pull.rebase = true;
+          color.ui = "auto";
+        };
       };
     };
-  };
 }
