d91f7381fe4b90e8c61c1e59792428f66944ada1
pimkoter <pimkoter69@gmail.com>
2026-09-03 19:10:22 +0200

    fix(dns) added Resolved-systemd again

--- modules/nixos/services.nix
+++ modules/nixos/services.nix
@@ -1,18 +1,26 @@
 {
   flake.nixosModules.services = { pkgs, ... }: {
     services = {
+      resolved = {
+        enable = true;
+        settings.Resolve = {
+          DNSSEC = "true";
+          Domains = [ "~." ];
+          DNSOverTLS = "true";
+          FallbackDNS = [
+            "1.1.1.1"
+            "1.0.0.1"
+          ];
+        };
+      };
       avahi = {
         enable = true;
         openFirewall = true;
       };
-      printing = {
-        enable = true;
-      };
+      printing.enable = true;
       openssh.enable = true;
       upower.enable = true;
-      xserver.xkb = {
-        layout = "us";
-      };
+      xserver.xkb.layout = "us";
       blueman.enable = true;
       tailscale = {
         enable = true;
@@ -26,35 +34,6 @@
     hardware.bluetooth = {
       enable = true;
       powerOnBoot = true;
-    };
-
-    # Auto enable tailscale on boot
-    systemd.services.tailscale-autoconnect = {
-      description = "Automatic connection to Tailscale";
-
-      # make sure tailscale is running before trying to connect to tailscale
-      after = [
-        "network-pre.target"
-        "tailscale.service"
-      ];
-      wants = [
-        "network-pre.target"
-        "tailscale.service"
-      ];
-      wantedBy = [ "multi-user.target" ];
-
-      # set this service as a oneshot job
-      serviceConfig.Type = "oneshot";
-
-      # have the job run this shell script
-      script = with pkgs; ''
-        sleep 2
-        status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
-        if [ $status = "Running" ]; then # if so, then do nothing
-          exit 0
-        fi
-        ${tailscale}/bin/tailscale up
-      '';
     };
   };
 }
